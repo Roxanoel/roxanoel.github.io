074c4e08297c4f9537b4ea3cd387bd1a9b862dbc
Roxanoel <roxane.noel92@gmail.com>
2022-09-03 20:09:16 +0100

    Basic navbar styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import "./scss/styles.scss";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 id='title'>Roxane Noel</h1>
-        <nav class="navbar">
-          
-        </nav>
+        <Navbar />
       </header>
     </div>
   );
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+function Navbar() {
+return (
+  <nav class="navbar">
+    <ul>
+      <li>Web</li>
+      <li>Games</li>
+      <li>Misc</li>
+    </ul>
+  </nav>
+)
+}
+
+export default Navbar;
