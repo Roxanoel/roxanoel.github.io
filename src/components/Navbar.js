2dcb68ac95d54856acd129606ec6c4b660139e77
Roxane Noël <roxane.noel92@gmail.com>
2024-01-09 12:04:05 -0800

    Added SVG logo to navbar
    no functionality yet but intention is to have it scroll back to the top

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,14 @@ import NavLink from "./Navlink";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faItchIo } from "@fortawesome/free-brands-svg-icons";
 import { faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import LogoSvg from "./LogoSvg";
 
 function Navbar() {
 return (
   <nav className="navbar">
     <ul>
-      <NavLink text="GITHUB" link="https://github.com/Roxanoel">
+      <LogoSvg width={32} height={32} />
+      {/*<NavLink text="GITHUB" link="https://github.com/Roxanoel">
        <FontAwesomeIcon icon={faGithub} />
       </NavLink>
       <NavLink text="LINKEDIN" link="https://www.linkedin.com/in/roxane-noel/"> 
@@ -15,7 +17,7 @@ return (
       </NavLink>
       <NavLink text="ITCH.IO" link="https://roxanoel.itch.io/"> 
         <FontAwesomeIcon icon={faItchIo} />
-      </NavLink>
+</NavLink>*/}
       <NavLink text="PROJECTS">
         <FontAwesomeIcon icon={faLaptopCode} />
       </NavLink>
