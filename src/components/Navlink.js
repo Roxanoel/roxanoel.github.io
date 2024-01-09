67c0a14a0a74f1810a1fa6dd40ff926be536758c
Roxane Noël <roxane.noel92@gmail.com>
2024-01-09 12:46:32 -0800

    icon right of navlink text

--- src/components/Navlink.js
+++ src/components/Navlink.js
@@ -1,8 +1,8 @@
 function NavLink({ link, text, children }) {
     return (
         <li className="navlink-container">
-            <span className="navlink-icon">{children}</span>
             <a className="navlink" href={link}>{text}</a>
+            <span className="navlink-icon">{children}</span>
         </li>
     )
 }
