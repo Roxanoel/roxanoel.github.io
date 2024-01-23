e60bf76cc7ca9859ef141bd5ea7d2685a3a53cf0
Roxane Noël <roxane.noel92@gmail.com>
2024-01-23 13:53:17 -0800

    Added images to cards

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,12 +1,20 @@
 function ProjectCard({name, description, tags, githubLink, liveLink, imageLink}) {
-    return <div className="project-card">
+   console.log(imageLink)
+   return <div className="project-card">
         <div className="project-card-text-container">
             <h4>{name}</h4>
             <p>{description}</p>
             {liveLink && <button>Live Demo</button>}
             {githubLink && <button>Learn More</button>}
         </div>
-        <div className="project-card-image" style={{backgroundImage: `url(${imageLink})`, backgroundColor: "red"}} />
+        <div 
+            className="project-card-image" 
+            style={{ 
+                backgroundImage: `url("${imageLink}")`, 
+                backgroundPosition: "center",
+                backgroundSize: "cover",
+            }}
+        />
     </div>
 }
 
