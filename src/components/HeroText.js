75cd417e61ec8fc4723cd2aea663d9e404efbc56
Roxane Noël <roxane.noel92@gmail.com>
2024-01-26 10:04:40 -0800

    Added temporary "under construction" messaging

--- src/components/HeroText.js
+++ src/components/HeroText.js
@@ -8,11 +8,16 @@ function HeroText() {
                 <p>
                 I like to build cool stuff with <strong>Vue</strong>, <strong>React</strong>, <strong>Unity</strong>, and sometimes my sewing machine. In a past life, I studied medieval philosophy at the University of Cambridge. 
                 </p>
+                {/*
                 <p>
                 I'm a <strong>driven</strong> and <strong>passionate</strong> creator who is not afraid of trying new things. Whatever the project, I'll bring on my good mood and a little extra pizzazz. 
                 </p>
                 <p>
                 Got a project you need help with? Want to know more about twelfth century philosophy? Wish you had someone to share raccoon memes with? Don't be a stranger, drop me a message! 💌
+                </p>
+                */}
+                <p>
+                This site is currently undergoing a makeover. Until the new and improved version is ready to release, do feel free to check out my LinkedIn, GitHub, and itch profiles to see some of what I've been up to!
                 </p>
             </div>
             {/*<div>
