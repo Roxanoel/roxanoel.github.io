bbd96b932eae8f65c12da9e2c3641f6b76c4bab0
Roxane Noël <roxane.noel92@gmail.com>
2024-11-10 14:14:20 -0800

    header component blurb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import "@/styles/global.css";
+import Header from "@/components/Header/Header";
 
 export default function Page() {
-  return (
-    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
-      <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-        Roxane Noël
-      </h1>
-    </header>
-  );
+  return <Header />;
 }
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,45 @@
+export default function Header() {
+  return (
+    <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+      <div>
+        <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
+          Roxane Noël
+        </h1>
+        <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl">
+          Front End Engineer
+        </h2>
+        <p className="mt-4 max-w-xs leading-normal">
+          Building systems and crafting impactful experiences.
+        </p>
+        <nav className="nav hidden lg:block" aria-label="In-page jump links">
+          <ul className="mt-16 w-max">
+            <li>
+              <a className="group flex items-center py-3 active" href="#about">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  About
+                </span>
+              </a>
+            </li>
+            <li>
+              <a className="group flex items-center py-3" href="#experience">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  Experience
+                </span>
+              </a>
+            </li>
+            <li>
+              <a className="group flex items-center py-3" href="#projects">
+                <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
+                <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
+                  Projects
+                </span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
